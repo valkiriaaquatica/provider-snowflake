c058566ac9a4554106b6e086ff1a2249ca490e9a
valkiriaaquatica <fernandomendietaovejero@gmail.com>
2025-09-05 23:06:30 +0200

    feat(resources): add warehouse resource

--- apis/zz_register.go
+++ apis/zz_register.go
@@ -10,7 +10,7 @@ package apis
 import (
 	"k8s.io/apimachinery/pkg/runtime"
 
-	v1alpha1 "github.com/valkiriaaquatica/provider-snowflake/apis/null/v1alpha1"
+	v1alpha1 "github.com/valkiriaaquatica/provider-snowflake/apis/snowflakewarehouse/v1alpha1"
 	v1alpha1apis "github.com/valkiriaaquatica/provider-snowflake/apis/v1alpha1"
 	v1beta1 "github.com/valkiriaaquatica/provider-snowflake/apis/v1beta1"
 )
--- config/provider.go
+++ config/provider.go
@@ -10,7 +10,7 @@ import (
 
 	ujconfig "github.com/crossplane/upjet/pkg/config"
 
-	"github.com/valkiriaaquatica/provider-snowflake/config/null"
+	"github.com/valkiriaaquatica/provider-snowflake/config/snowflakewarehouse"
 )
 
 const (
@@ -36,7 +36,7 @@ func GetProvider() *ujconfig.Provider {
 
 	for _, configure := range []func(provider *ujconfig.Provider){
 		// add custom config functions
-		null.Configure,
+		snowflakewarehouse.Configure,
 	} {
 		configure(pc)
 	}
